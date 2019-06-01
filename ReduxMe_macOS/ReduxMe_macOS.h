92392681eccb5e032d4a2a6fe334ce2c50ed315e
Ilias Pavidakis <ipavlidakis@gmail.com>
2019-06-01 18:41:21 +0100

    Adding mac and tv targets

---
+++ ReduxMe_macOS/ReduxMe_macOS.h
@@ -0,0 +1,13 @@
+//
+//  ReduxMe_macOS.h
+//  ReduxMe_macOS
+//
+//  Created by Ilias Pavlidakis on 01/06/2019.
+//  Copyright © 2019 Ilias Pavlidakis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReduxMe_macOS : NSObject
+
+@end
